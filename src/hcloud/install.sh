9dd1e124901cf7fe8c972e65065fd371030fe5ac
Daniel Marohn <daniel.marohn@googlemail.com>
2023-04-11 03:52:28 +0200

    Simplify getting download url

--- src/hcloud/install.sh
+++ src/hcloud/install.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
+hcloud_package="hcloud-linux-amd64.tar.gz"
 download_dir=$(mktemp -d)
-hcloud_version=$(curl -s -I https://github.com/hetznercloud/cli/releases/latest/ | grep -Fi location: | egrep -o 'v[0-9.]+')
-echo "download version ${hcloud_version}"
-download_url="https://github.com/hetznercloud/cli/releases/download/${hcloud_version}/hcloud-linux-amd64.tar.gz"
-wget ${download_url} --directory-prefix=${download_dir} --quiet
-    
-tar zxf ${download_dir}/hcloud-linux-amd64.tar.gz --directory ${download_dir}
+
+wget https://github.com/hetznercloud/cli/releases/latest/download/${hcloud_package} --directory-prefix=${download_dir} --quiet    
+tar zxf ${download_dir}/${hcloud_package} --directory ${download_dir}
 install ${download_dir}/hcloud /usr/local/bin
-echo "cleanup..."
+
 rm -rf $download_dir
 
 echo installed $(hcloud version)
